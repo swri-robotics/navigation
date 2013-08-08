ca9269e19006cd3ffb1e9bee8c8d9fcb4dd1d9bf
David Lu!! <davidvlu@gmail.com>
2013-08-08 12:24:19 -0500

    Better raytracing for both obstacle and voxel layers

--- costmap_2d/plugins/obstacle_layer.cpp
+++ costmap_2d/plugins/obstacle_layer.cpp
@@ -462,15 +462,7 @@ void ObstacleLayer::raytraceFreespace(const Observation& clearing_observation, d
     //and finally... we can execute our trace to clear obstacles along that line
     raytraceLine(marker, x0, y0, x1, y1, cell_raytrace_range);
 
-    double dx = wx-ox, dy = wy-oy;
-    double full_distance = sqrt( dx*dx+dy*dy );
-    double scale = std::min(1.0, clearing_observation.raytrace_range_ / full_distance);
-    double ex = ox + dx * scale, ey = oy + dy * scale;
-    *min_x = std::min(ex, *min_x);
-    *min_y = std::min(ey, *min_y);
-    *max_x = std::max(ex, *max_x);
-    *max_y = std::max(ey, *max_y);
-
+    updateRaytraceBounds(ox, oy, wx, wy, clearing_observation.raytrace_range_, min_x, min_y, max_x, max_y);
   }
 }
 
@@ -497,6 +489,20 @@ void ObstacleLayer::deactivate()
       observation_subscribers_[i]->unsubscribe();
   }
 }
+
+void ObstacleLayer::updateRaytraceBounds(double ox, double oy, double wx, double wy, double range, double* min_x, double* min_y,
+					 double* max_x, double* max_y)
+{
+  double dx = wx-ox, dy = wy-oy;
+  double full_distance = sqrt( dx*dx+dy*dy );
+  double scale = std::min(1.0, range / full_distance);
+  double ex = ox + dx * scale, ey = oy + dy * scale;
+  *min_x = std::min(ex, *min_x);
+  *min_y = std::min(ey, *min_y);
+  *max_x = std::max(ex, *max_x);
+  *max_y = std::max(ey, *max_y);
+}
+
 
 void ObstacleLayer::onFootprintChanged()
 {
