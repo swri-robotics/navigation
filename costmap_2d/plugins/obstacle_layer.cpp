2db72145ecdae8386f575aa897743e1934bf003f
David Lu!! <davidvlu@gmail.com>
2013-08-08 12:11:39 -0500

    Better bounds checking for raytracing

--- costmap_2d/plugins/obstacle_layer.cpp
+++ costmap_2d/plugins/obstacle_layer.cpp
@@ -457,15 +457,20 @@ void ObstacleLayer::raytraceFreespace(const Observation& clearing_observation, d
     if (!worldToMap(wx, wy, x1, y1))
       continue;
 
-    *min_x = std::min(wx, *min_x);
-    *min_y = std::min(wy, *min_y);
-    *max_x = std::max(wx, *max_x);
-    *max_y = std::max(wy, *max_y);
-
     unsigned int cell_raytrace_range = cellDistance(clearing_observation.raytrace_range_);
     MarkCell marker(costmap_, FREE_SPACE);
     //and finally... we can execute our trace to clear obstacles along that line
     raytraceLine(marker, x0, y0, x1, y1, cell_raytrace_range);
+
+    double dx = wx-ox, dy = wy-oy;
+    double full_distance = sqrt( dx*dx+dy*dy );
+    double scale = std::min(1.0, clearing_observation.raytrace_range_ / full_distance);
+    double ex = ox + dx * scale, ey = oy + dy * scale;
+    *min_x = std::min(ex, *min_x);
+    *min_y = std::min(ey, *min_y);
+    *max_x = std::max(ex, *max_x);
+    *max_y = std::max(ey, *max_y);
+
   }
 }
 
